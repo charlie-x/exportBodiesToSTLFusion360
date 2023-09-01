31b03b70987a3f4437ef0687af743f3d4d6492d6
charliex <charlie-x@users.noreply.github.com>
2023-08-31 23:47:08 -0700

    Update export_stl.py
    tidy imports

--- export_stl.py
+++ export_stl.py
@@ -2,7 +2,7 @@
 #Description- export bodies to individual STLs
 
 # import the required libraries
-import adsk.core, adsk.fusion, adsk.cam, traceback, os
+import adsk.core, adsk.fusion, os
 
 def run(context):
     ui = None
